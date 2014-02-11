3bfc0cf2a294c36764664ea43fa440fd8893157e
bstammers <brad.stammers@iinet.net.au>
2014-02-11 16:35:21 +1000

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -16,7 +16,7 @@ describe "UserPages" do
 		before {visit signup_path}
 		let(:submit) {"Create my account"}
 
-		describe "with invlid information" do
+		describe "with invalid information" do
 			it "should not create a user" do
 				expect {click_button submit}.not_to change(User, :count)
 			end
@@ -32,6 +32,14 @@ describe "UserPages" do
 
 			it "should create a user" do
 				expect {click_button submit}.to change(User, :count)
+			end
+
+			describe "after saving the user" do
+				before {click_button submit}
+				let(:user) {User.find_by(email: 'user@example.com')}
+				it {should have_link('Sign out')}
+				it {should have_title(user.name)}
+				it {should have_selector('div.alert.alert-success', text: "Welcome")}
 			end
 		end
 	end
